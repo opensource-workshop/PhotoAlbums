a96afa2c059dfa22288268793ed2593a1dbeb88a
kteraguchi <kteraguchi@commonsnet.org>
2016-03-11 14:20:22 +0900

    Modify to use plugin app model

--- Model/PhotoAlbumPhoto.php
+++ Model/PhotoAlbumPhoto.php
@@ -8,7 +8,7 @@
 * @license  http://www.netcommons.org/license.txt NetCommons License
  */
 
-App::uses('AppModel', 'Model');
+App::uses('PhotoAlbumsAppModel', 'PhotoAlbums.Model');
 
 /**
  * Summary for PhotoAlbumPhoto Model
