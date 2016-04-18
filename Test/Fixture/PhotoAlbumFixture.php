eaae5c62e2138c23c934c01badfa1e7fcc7167a2
kteraguchi <kteraguchi@commonsnet.org>
2016-04-18 18:19:56 +0900

    Fix test comment

--- Test/Fixture/PhotoAlbumFixture.php
+++ Test/Fixture/PhotoAlbumFixture.php
@@ -2,9 +2,10 @@
 /**
  * PhotoAlbumFixture
  *
-* @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
-* @link     http://www.netcommons.org NetCommons Project
-* @license  http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ * @author Kohei Teraguchi <kteraguchi@commonsnet.org>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
  */
 
 /**
