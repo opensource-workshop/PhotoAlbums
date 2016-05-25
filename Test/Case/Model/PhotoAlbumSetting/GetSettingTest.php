bd90461f3fe0aa02737ff3be046ae3b9f5cf29dc
kteraguchi <kteraguchi@commonsnet.org>
2016-05-25 13:23:16 +0900

    Implement PhotoAlbumSetting::getSetting test

---
+++ Test/Case/Model/PhotoAlbumSetting/GetSettingTest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * PhotoAlbumSettingGetSetting Test Case
+ *
+ * @copyright Copyright 2014, NetCommons Project
+ * @author Kohei Teraguchi <kteraguchi@commonsnet.org>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ */
+
+App::uses('PhotoAlbumSetting', 'PhotoAlbums.Model');
+App::uses('PhotoAlbumTestCurrentUtil', 'PhotoAlbums.Test/Case/Model');
+
+/**
+ * Summary for PhotoAlbumSettingGetSetting Test Case
+ */
+class PhotoAlbumSettingGetSettingTest extends CakeTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.photo_albums.photo_album_setting',
+	);
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$this->PhotoAlbumSetting = ClassRegistry::init('PhotoAlbums.PhotoAlbumSetting');
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		unset($this->PhotoAlbumSetting);
+
+		parent::tearDown();
+	}
+
+/**
+ * getFrameSetting test
+ *
+ * @return void
+ */
+	public function testGetSetting() {
+		$currentValue['Block']['key'] = 'Lorem ipsum dolor sit amet';
+		PhotoAlbumTestCurrentUtil::setValue($currentValue);
+
+		$expected = (new PhotoAlbumSettingFixture())->records[0];
+		$actual = $this->PhotoAlbumSetting->getSetting();
+		$this->assertEquals($expected, $actual['PhotoAlbumSetting']);
+
+		PhotoAlbumTestCurrentUtil::setOriginValue();
+	}
+
+/**
+ * getFrameSettingNotFound test
+ *
+ * @return void
+ */
+	public function testGetFrameSettingNotFound() {
+		$actual = $this->PhotoAlbumSetting->getSetting();
+
+		$this->assertArrayHasKey('PhotoAlbumSetting', $actual);
+		$this->assertArrayNotHasKey('id', $actual['PhotoAlbumSetting']);
+	}
+}
