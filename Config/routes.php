d782a30819f3fe5b9b084671ca3c05d9a54885e8
s-nakajima <nakajimashouhei@gmail.com>
2016-07-18 08:35:49 +0900

    ルーティングの見直し https://github.com/NetCommons3/NetCommons3/issues/309 https://github.com/NetCommons3/NetCommons/pull/284

--- Config/routes.php
+++ Config/routes.php
@@ -11,7 +11,10 @@
 Router::connect(
 	'/photo_albums/photo_album_photos/photo/:block_id/:key/:content_id/:size',
 	array('plugin' => 'photo_albums', 'controller' => 'photo_album_photos', 'action' => 'photo'),
-	array('block_id' => '[0-9]+', 'content_id' => '[0-9]+', 'size' => 'big|medium|small|thumb')
+	array(
+		'block_id' => '[0-9]+', 'key' => '[0-9a-zA-Z_]', 'content_id' => '[0-9]+',
+		'size' => 'big|medium|small|thumb'
+	)
 );
 
 Router::connect(
@@ -20,11 +23,11 @@ Router::connect(
 		'plugin' => 'photo_albums', 'controller' => 'photo_album_photos',
 		'action' => 'photo', 'size' => 'big'
 	),
-	array('block_id' => '[0-9]+', 'content_id' => '[0-9]+')
+	array('block_id' => '[0-9]+', 'key' => '[0-9a-zA-Z_]', 'content_id' => '[0-9]+')
 );
 
 Router::connect(
 	'/photo_albums/photo_albums/jacket/:block_id/:key/:field_name/:size',
 	array('plugin' => 'photo_albums', 'controller' => 'photo_albums', 'action' => 'jacket'),
-	array('block_id' => '[0-9]+', 'size' => 'big|medium|small|thumb')
+	array('block_id' => '[0-9]+', 'key' => '[0-9a-zA-Z_]', 'size' => 'big|medium|small|thumb')
 );
